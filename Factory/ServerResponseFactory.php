e92dd54715ee9bee27cf68d2c795ebc96f42e9e3
soen <970852318@qq.com>
2020-05-19 18:32:22 +0800

    add

---
+++ Factory/ServerResponseFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Soen\Http\Message\Factory;
+
+use Soen\Http\Message\Response;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class ServerResponseFactory
+ * @package Soen\Http\Message\Factory
+ */
+class ServerResponseFactory implements ResponseFactoryInterface
+{
+
+    /**
+     * Create a new response.
+     *
+     * @param int $code HTTP status code; defaults to 200
+     * @param string $reasonPhrase Reason phrase to associate with status code
+     *     in generated response; if none is provided implementations MAY use
+     *     the defaults as suggested in the HTTP specification.
+     *
+     * @return ResponseInterface
+     */
+    public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface
+    {
+        return new Response($code, $reasonPhrase);
+    }
+
+    /**
+     * Create a new ResponseInterface.
+     *
+     * @param \Swoole\Http\Response $response
+     * @return ResponseInterface
+     */
+    public function createResponseFromSwoole(\Swoole\Http\Response $resp): ResponseInterface
+    {
+        /** @var Response $response */
+        $response = $this->createResponse();
+        $response->withSwooleResponse($resp);
+        return $response;
+    }
+
+}
